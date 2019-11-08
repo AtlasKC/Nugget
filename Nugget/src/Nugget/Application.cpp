49febfa356f896b11e8bb35ba0d57b2f06f7e7d0
AtlasKC <kadenc28@gmail.com>
2019-11-08 12:44:47 -0500

    Now dispatching ApplicationEvents with GLFW callbacks.

--- Nugget/src/Nugget/Application.cpp
+++ Nugget/src/Nugget/Application.cpp
@@ -1,15 +1,17 @@
 #include "nepch.h"
 #include "Application.h"
-
-#include "Nugget/Events/ApplicationEvent.h"
 #include "Nugget/Log.h"
 #include <GLFW/glfw3.h>
 
 namespace Nugget
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -17,13 +19,26 @@ namespace Nugget
 		
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		NE_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		while(m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0, 1, 0.5, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Nugget/src/Nugget/Application.h
+++ Nugget/src/Nugget/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Nugget/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Nugget
@@ -12,7 +13,9 @@ namespace Nugget
 		Application();
 		virtual ~Application();
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
